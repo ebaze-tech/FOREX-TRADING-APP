0a561463f9fcf350906345dede12c05e4de6a5e0
ebaze-tech <139893207+ebaze-tech@users.noreply.github.com>
2024-08-05 09:32:57 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,11 +5,15 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const authRoutes = require("./routes/AuthRoutes");
-// const postRoutes = require("./routes/PostRoutes");
 const passwordRoutes = require("./routes/PasswordRoutes");
-// const subscriptionRoutes = require("./routes/SubscriptionRoutes");
 const subscriptionRoutes = require("./routes/SubscriptionRoutes");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+
+dotenv.config();
+
 const app = express();
 
 // Middleware
@@ -18,20 +22,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const { JWT_SECRET } = process.env;
-dotenv.config();
+// Security Middleware
+app.use(helmet());
+
+// Logging Middleware
+app.use(morgan("combined"));
 
 const allowedOrigins = ["https://forex-trading-git-master-toluwanimi-faramades-projects.vercel.app/"];
 
 // Session middleware
 app.use(
   session({
-    secret: process.env.JWT_SECRET, // Replace with your own secret key
+    secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: true, // true in production
+      secure: true, // Ensure this is true for production
       httpOnly: true,
+      sameSite: "strict", // Prevent CSRF attacks
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
     },
   })
 );
@@ -41,6 +50,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./config/passport.config")(passport);
+
 // CORS
 const corsOptions = {
   origin: function (origin, callback) {
@@ -59,26 +69,29 @@ app.use(cors(corsOptions));
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/post", postRoutes);
 app.use("/api/auth", passwordRoutes);
-// app.use("/api/subscription", subscriptionRoutes);
 app.use("/api/subscription", subscriptionRoutes);
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Error handling
 app.use((err, req, res, next) => {
+  console.error(err.stack);
   res.status(err.status || 500);
-  res.json({ error: err.message });
+  res.json({ error: "Internal Server Error" });
 });
 
 const PORT = process.env.PORT || 8080;
 
 // Database and server connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-    console.log("MongoDB connected. ");
+    console.log("MongoDB connected.");
   })
   .catch((error) => {
     console.error("Connection error", error.message);
