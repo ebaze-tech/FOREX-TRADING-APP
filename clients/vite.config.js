ab03ae67cd346b15046b56c8893a3f6e7e413522
ebaze-tech <ebaze.technologies@gmail.com>
2024-08-05 01:11:01 +0100

    Updating

--- clients/vite.config.js
+++ clients/vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables based on the mode (development, production, etc.)
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    define: {
-      "process.env.REACT_API_URL": JSON.stringify(env.REACT_APP_API_URL),
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  // Environment variables are automatically loaded by Vite
+  define: {
+    'process.env.REACT_API_URL': JSON.stringify(process.env.VITE_API_URL),
+  },
 });
